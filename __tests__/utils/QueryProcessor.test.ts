8b133bad67f146fd5e20dd99804393a81a1b5bd3
Jessica Zheng <jzheng5@andrew.cmu.edu>
2026-02-23 10:21:13 -0500

    Added tests for andrewID and edited name

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -22,7 +22,15 @@ describe("QueryProcessor", () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "Rohan"
+            "Jessica Zheng"
+          ));
+    })
+
+    test('should return andrewid', () => {
+        const query = "What is your Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "jzheng5"
           ));
     })
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,11 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Jessica Zheng";
+  }
+
+  if (query.toLowerCase().includes("andrew id")) {
+    return "jzheng5";
   }
 
   return "";
